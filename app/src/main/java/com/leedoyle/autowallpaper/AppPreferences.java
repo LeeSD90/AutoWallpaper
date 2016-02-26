31344dca2deba4d2ea4c7c4a40a6a58680f390e1
LeeSD90 <lee.doyle90@gmail.com>
2016-02-26 18:45:32 +0000

    Removed an old testing TODO

--- app/src/main/java/com/leedoyle/autowallpaper/AppPreferences.java
+++ app/src/main/java/com/leedoyle/autowallpaper/AppPreferences.java
@@ -101,7 +101,7 @@ public class AppPreferences extends PreferenceFragment implements SharedPreferen
         int n = ran.nextInt(90000);
         String fileName = "Image_" + n + ".jpg";
         File file = new File(wallpaperDir, fileName);
-        while(file.exists()) file = new File(wallpaperDir, "Image_" + ran.nextInt(90000) + ".jpg"); //TODO test this
+        while(file.exists()) file = new File(wallpaperDir, "Image_" + ran.nextInt(90000) + ".jpg");
         FileOutputStream fos = new FileOutputStream(file);
         try{
             wallpaper.compress(Bitmap.CompressFormat.JPEG, 90, fos);
