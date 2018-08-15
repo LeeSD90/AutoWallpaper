fb064e91e4e63aa9fef77d96671be745781a25fb
LeeSD90 <lee.doyle90@gmail.com>
2018-08-15 09:31:38 +0200

    Remove unused options menu code

--- app/src/main/java/com/example/lee/autowallpaper_rewrite/Preview.java
+++ app/src/main/java/com/example/lee/autowallpaper_rewrite/Preview.java
@@ -74,21 +74,6 @@ public class Preview extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -117,6 +102,7 @@ public class Preview extends AppCompatActivity
         return true;
     }
 
+    // TODO Own thread?
     // Get a new wallpaper with the given settings
     private void getNewWallpaper() {
         try{
