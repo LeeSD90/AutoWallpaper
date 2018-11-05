857f3fab940621c5cfb584076d6cc593fa889bcc
LeeSD90 <lee.doyle90@gmail.com>
2018-11-05 11:43:18 +0100

    Move refresh service to own process

--- app/src/main/java/com/example/lee/autowallpaper_rewrite/WallpaperSetter.java
+++ app/src/main/java/com/example/lee/autowallpaper_rewrite/WallpaperSetter.java
@@ -3,7 +3,6 @@ package com.example.lee.autowallpaper_rewrite;
 import android.app.WallpaperManager;
 import android.content.Context;
 
-// TODO broadcast receiver?
 public class WallpaperSetter {
 
     private WallpaperSetter(){
