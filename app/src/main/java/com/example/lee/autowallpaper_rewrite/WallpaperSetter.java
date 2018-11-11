ed532cbc0a9959e5b2040ab5d8406e793c1bf936
LeeSD90 <lee.doyle90@gmail.com>
2018-11-11 12:04:13 +0100

    Simplify search string before adding extra parameters

--- app/src/main/java/com/example/lee/autowallpaper_rewrite/WallpaperSetter.java
+++ app/src/main/java/com/example/lee/autowallpaper_rewrite/WallpaperSetter.java
@@ -12,7 +12,7 @@ public class WallpaperSetter {
     // Get a new wallpaper with the given settings
     public static Boolean setNewWallpaper(Context c, String search) {
         try{
-            WallpaperManager.getInstance(c).setBitmap(ImageParser.getWallpaper("https://www.google.no/search?q=" + search + "&safe=off&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiW3tSZpZLcAhXDBZoKHcggB10Q_AUICigB&biw=2560&bih=1307"));
+            WallpaperManager.getInstance(c).setBitmap(ImageParser.getWallpaper("https://www.google.no/search?tbm=isch&q=" + search));
         }
         catch (Exception e){
             e.printStackTrace();
