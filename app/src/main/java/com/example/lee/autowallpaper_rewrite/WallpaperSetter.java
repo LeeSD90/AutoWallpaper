06a9f0c21732cb01b55b97ca185a91adea64de02
LeeSD90 <lee.doyle90@gmail.com>
2018-11-12 13:34:48 +0100

    Add hardcoded url image settings for mobile friendly results

--- app/src/main/java/com/example/lee/autowallpaper_rewrite/WallpaperSetter.java
+++ app/src/main/java/com/example/lee/autowallpaper_rewrite/WallpaperSetter.java
@@ -14,7 +14,12 @@ public class WallpaperSetter {
     // Get a new wallpaper with the given settings
     public static Boolean setNewWallpaper(Context c, HashMap<String, String> settings) {
         try{
-            WallpaperManager.getInstance(c).setBitmap(ImageParser.getWallpaper("https://www.google.no/search?tbm=isch&q=" + settings.get("Search")));
+            String search = settings.get("Search");
+            WallpaperManager.getInstance(c).setBitmap(ImageParser.getWallpaper("https://www.google.no/search?tbm=isch&q=" + search //Search String
+                                                                                            + "&tbs="
+                                                                                            + "iar:t," //Aspect Ratio
+                                                                                            + "isz:lt,islt:qsvga" //Image size
+            ));
         }
         catch (Exception e){
             e.printStackTrace();
